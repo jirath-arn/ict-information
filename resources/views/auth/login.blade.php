8e4f9406ff89943711261506fdbe81c7e3e22d44
Jirath Arnamnath <ice191913@gmail.com>
2024-06-29 10:33:24 +0700

    Updated Views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title', 'Login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<main class="container flex items-center justify-center py-10">
+    <div class="w-full md:w-1/2 xl:w-1/3">
+        <div class="mx-5 md:mx-10">
+            <h2 class="uppercase">ICT-Information</h2>
+            <h4 class="uppercase">Rmutto</h4>
         </div>
+
+        <form action="#" class="card mt-5 p-5 md:p-10">
+            {{-- Username --}}
+            <div class="mb-5">
+                <label for="username" class="label block mb-2">
+                    Username
+                </label>
+
+                <input id="username" type="text" class="form-control" placeholder="XXXXXXXXXXXX-X" required />
+            </div>
+
+            {{-- Password --}}
+            <div class="mb-5">
+                <label for="password" class="label block mb-2">
+                    Password
+                </label>
+
+                <input id="password" type="password" class="form-control" required />
+            </div>
+
+            {{-- Login Button --}}
+            <div class="flex items-center">
+                <button class="btn btn_primary mx-auto uppercase">
+                    Login
+                </button>
+            </div>
+        </form>
     </div>
-</div>
+</main>
 @endsection
