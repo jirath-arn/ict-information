f3d0886e7bd2c12f13686ee7a7acd7984ac09ce0
Jirath Arnamnath <ice191913@gmail.com>
2024-06-29 22:57:34 +0700

    Updated Route

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Auth\LoginController;
-
-Route::get('/', [LoginController::class, 'showLoginForm']);
-
 Auth::routes(['register' => false]);
+
+Route::redirect('/', '/login');
 
 Route::group(['middleware' => ['auth']], function () {
     //
