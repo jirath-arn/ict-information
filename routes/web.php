a70845c35fab6077d203de81786d5bac7cb8278e
Jirath Arnamnath <ice191913@gmail.com>
2024-07-01 01:14:06 +0700

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Auth::routes(['register' => false]);
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\CRUDs\DashboardController;
+use App\Http\Controllers\CRUDs\StudentInformationController;
 
 Route::redirect('/', '/login');
+Route::redirect('/logout', '/login');
 
-Route::group(['middleware' => ['auth']], function () {
-    //
+Route::middleware(['guest'])->group(function () {
+    // Auth.
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    // Auth.
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::middleware(['student'])->group(function () {
+        // Student Information.
+        Route::get('student_information', [StudentInformationController::class, 'index'])->name('student_information');
+    });
+
+    Route::middleware(['teacher'])->group(function () {
+        // Dashboard.
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    });
 });
