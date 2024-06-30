96aa1f4e9284b710a9bd60d23f7beb435592ff51
Jirath Arnamnath <ice191913@gmail.com>
2024-06-30 11:02:32 +0700

    Updated auth controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,39 +2,49 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\Role;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
+    protected function redirectTo()
+    {
+        $role = auth()->user()->role->value;
+
+        if ($role === Role::STUDENT) {
+            return '/student_information';
+
+        } else if ($role === Role::TEACHER) {
+            return '/dashboard';
+
+        } else if ($role === Role::ADMIN) {
+            return '/dashboard';
+
+        } else {
+            return '/';
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
     }
 }
