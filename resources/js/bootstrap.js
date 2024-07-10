8edb3efd5581aa13ade9051322734950fd982223
Jirath Arnamnath <ice191913@gmail.com>
2024-07-11 00:01:35 +0700

    Add jQuery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
-window.axios = axios;
+import jQuery from 'jquery';
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.$ = jQuery;
