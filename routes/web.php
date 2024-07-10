1ec2d7629744c46e181cb37e16f16d621f19b612
Jirath Arnamnath <ice191913@gmail.com>
2024-07-11 04:34:06 +0700

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CRUDs\DashboardController;
+use App\Http\Controllers\CRUDs\EducationInformationController;
+use App\Http\Controllers\CRUDs\FamilyInformationController;
+use App\Http\Controllers\CRUDs\PersonalInformationController;
+use App\Http\Controllers\CRUDs\ProfileController;
 use App\Http\Controllers\CRUDs\StudentInformationController;
+use App\Http\Controllers\CRUDs\StudentManagementController;
 
 Route::redirect('/', '/login');
 Route::redirect('/logout', '/login');
@@ -19,13 +24,28 @@ Route::middleware(['auth'])->group(function () {
     // Auth.
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
+    // Profile.
+    Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
     Route::middleware(['student'])->group(function () {
         // Student Information.
         Route::get('student_information', [StudentInformationController::class, 'index'])->name('student_information');
+
+        // Personal Information.
+        Route::get('personal_information', [PersonalInformationController::class, 'index'])->name('personal_information');
+        
+        // Family Information.
+        Route::get('family_information', [FamilyInformationController::class, 'index'])->name('family_information');
+        
+        // Education Information.
+        Route::get('education_information', [EducationInformationController::class, 'index'])->name('education_information');
     });
 
     Route::middleware(['teacher'])->group(function () {
         // Dashboard.
         Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+        // Student Management.
+        Route::get('student_management', [StudentManagementController::class, 'index'])->name('student_management');
     });
 });
