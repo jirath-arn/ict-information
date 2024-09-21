05c0f0b4a83e0201e65808bd25c23439c0a88133
Jirath Arnamnath <ice191913@gmail.com>
2024-09-22 03:35:47 +0700

    Updated Enum

---
+++ app/Enums/Income.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+class Income extends Enum
+{
+    const LOW = 'LOW';
+    const MEDIUM = 'MEDIUM';
+    const HIGH = 'HIGH';
+}
---
+++ app/Enums/Life.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+class Life extends Enum
+{
+    const YES = 'YES';
+    const NO = 'NO';
+}
