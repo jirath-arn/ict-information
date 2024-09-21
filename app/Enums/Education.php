6ec2be1d54601d9ec1ee1f85308f6001e2ef0e1c
Jirath Arnamnath <ice191913@gmail.com>
2024-09-22 03:03:22 +0700

    Updated Enum

---
+++ app/Enums/Education.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+class Education extends Enum
+{
+    const SECONDARY_3 = 'SECONDARY_3';
+    const SECONDARY_6 = 'SECONDARY_6';
+    const VOC_CERT = 'VOC_CERT';
+    const HIGH_VOC_CERT = 'HIGH_VOC_CERT';
+    const BACHELORS_DEGREE = 'BACHELORS_DEGREE';
+    const MASTERS_DEGREE = 'MASTERS_DEGREE';
+    const PHD = 'PHD';
+}
