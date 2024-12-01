1fe15a1d8cad379fe876cc7f3fa00d40211f6a0e
Jirath Arnamnath <ice191913@gmail.com>
2024-12-01 15:20:43 +0700

    Updated Database

--- database/migrations/0001_01_01_000005_create_personal_information_table.php
+++ database/migrations/0001_01_01_000005_create_personal_information_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->smallInteger('weight')->nullable();
             $table->smallInteger('height')->nullable();
             $table->string('email', length: 70)->unique()->nullable();
-            $table->enum('scholarship', [Scholarship::YES, Scholarship::NO])->default(Scholarship::NO)->nullable();
+            $table->enum('scholarship', [Scholarship::YES, Scholarship::NO])->default(Scholarship::NO);
             $table->string('disability')->nullable();
             $table->enum('blood_type', [BloodType::A, BloodType::AB, BloodType::B, BloodType::O])->nullable();
             $table->enum('religion', [Religion::BUDDHISM, Religion::CHRISTIANITY, Religion::ISLAM, Religion::HINDUISM, Religion::JUDAISM, Religion::SIKHISM])->nullable();
